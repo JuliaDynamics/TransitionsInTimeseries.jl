fb15783b6f89c2a9dc995e54d1ce59f52c5d4a9f
JanJereczek <jan.jereczek@gmail.com>
2022-10-18 17:16:57 +0200

    corrected include chronology

--- src/TransitionIdentifiers.jl
+++ src/TransitionIdentifiers.jl
@@ -3,9 +3,9 @@ module TransitionIdentifiers
 using BenchmarkTools, CUDA, FFTW, StatsBase, LinearAlgebra
 
 include("utils.jl")
+include("signal_processing.jl")
 include("smoothing_kernels.jl")
 include("statistical_estimators.jl")
-include("signal_processing.jl")
 include("significance.jl")
 include("highlevel_TI_computation.jl")
 include("benchmark.jl")
