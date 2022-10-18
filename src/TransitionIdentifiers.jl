4fbc09753f36f784425962ffe01a58012a66b70b
JanJereczek <jan.jereczek@gmail.com>
2022-10-18 17:05:04 +0200

    including pkgs and src in TransitionIdentifiers.jl

--- src/TransitionIdentifiers.jl
+++ src/TransitionIdentifiers.jl
@@ -1,5 +1,15 @@
 module TransitionIdentifiers
 
-greet() = print("Hello World!")
+using BenchmarkTools, CUDA, FFTW, StatsBase, LinearAlgebra
+
+include("utils.jl")
+include("smoothing_kernels.jl")
+include("statistical_estimators.jl")
+include("signal_processing.jl")
+include("significance.jl")
+include("highlevel_TI_computation.jl")
+include("benchmark.jl")
+
+export skw
 
 end # module TransitionIdentifiers
