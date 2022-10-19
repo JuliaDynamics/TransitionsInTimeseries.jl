3268b3187abc267007acb2af0246b9a82489a155
JanJereczek <jan.jereczek@gmail.com>
2022-10-19 10:42:45 +0200

    ridge_regression passes test

--- test/test_statistical_identifiers.jl
+++ test/test_statistical_identifiers.jl
@@ -12,11 +12,12 @@ end
 function generate_ar1(x0::Vector{T}, p::Vector{T}, t::Vector{T}) where {T}
     tspan = extrema(t)
     prob = SDEProblem(f_linear!, g_whitenoise!, x0, tspan, p)
-    sol = solve(prob)
+    sol = solve(prob, EM(), dt=t[2]-t[1])
+
     return hcat(sol.(t)...)
 end
 
-@testset begin
+@testset "ar1_whitenoise" begin
     T = Float32
     dt = T(1e-2) 
     t = collect(T(0):dt:T(10))
@@ -30,16 +31,30 @@ end
     λ_est_mat = ar1_whitenoise(x_mat)[1]
     λ_est_gpu = Array( ar1_whitenoise(x_gpu) )[1]
     λ = exp(dt * p[1])
-    λ_list = [λ_est_vec, λ_est_mat, λ_est_gpu, λ]
-    println(λ_list)
+    # println([λ_est_vec, λ_est_mat, λ_est_gpu, λ])
     @test isapprox(λ_est_vec, λ_est_mat)
     @test isapprox(λ_est_gpu, λ_est_mat)
     @test isapprox(λ_est_gpu, λ, atol = T(1e-2))
 end
 
-@testset begin
-    dt = 1f-2
-    t = collect(0f0:dt:10f0)
-    tspan = extrema(t)
-    a = rand()
+function generate_affine_data(t::Vector{T}, nr::Int) where {T}
+    Y = fill(T(NaN), (nr, length(t))) # init test data.
+    W = 10 .* randn(2, nr)          # random parameters of affine function.
+    for i in 1:nr
+        Y[i, :] = W[1, i] .* t .+ W[2, i]
+    end
+    return Y, W
+end
+
+@testset "ridge_regression" begin
+    T = Float32
+    dt = T(1e-2) 
+    t = collect(T(0):dt:T(10))
+    nr = 10                         # number of tested regressions.
+    Y, W = generate_affine_data(t, nr)
+    Wcpu = ridge_regression(Y, t)
+    Wgpu = Array( ridge_regression(CuArray(Y), t) )
+    # println([W, Wcpu, Wgpu])
+    @test sum(isapprox.(W, Wcpu, atol = T(1e-5))) == 2*nr
+    @test sum(isapprox.(W, Wgpu, atol = T(1e-5))) == 2*nr
 end
