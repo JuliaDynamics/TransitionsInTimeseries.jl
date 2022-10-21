60d34757ca77047e50eef46d22f7ac0ec24328fa
JanJereczek <jan.jereczek@gmail.com>
2022-10-21 09:25:21 +0200

    percentile significance passes test

--- test/test_core.jl
+++ test/test_core.jl
@@ -13,7 +13,6 @@ function generate_ar1(x0::Vector{T}, p::Vector{T}, t::Vector{T}) where {T}
     tspan = extrema(t)
     prob = SDEProblem(f_linear!, g_whitenoise!, x0, tspan, p)
     sol = solve(prob, EM(), dt=t[2]-t[1])
-
     return hcat(sol.(t)...)
 end
 
@@ -27,19 +26,19 @@ end
     x_mat = generate_ar1(x0, p, t)
     x_vec = vec(x_mat)
     x_gpu = CuArray(x_mat)
-    λ_est_vec = ar1_whitenoise(x_vec)
-    λ_est_mat = ar1_whitenoise(x_mat)[1]
-    λ_est_gpu = Array( ar1_whitenoise(x_gpu) )[1]
-    λ = exp(dt * p[1])
-    # println([λ_est_vec, λ_est_mat, λ_est_gpu, λ])
-    @test isapprox(λ_est_vec, λ_est_mat)
-    @test isapprox(λ_est_gpu, λ_est_mat)
-    @test isapprox(λ_est_gpu, λ, atol = T(1e-2))
+    θ_est_vec = ar1_whitenoise(x_vec)
+    θ_est_mat = ar1_whitenoise(x_mat)[1]
+    θ_est_gpu = Array( ar1_whitenoise(x_gpu) )[1]
+    θ = exp(dt * p[1])          # θ is time discrete equivalent of λ
+
+    @test isapprox(θ_est_vec, θ_est_mat)
+    @test isapprox(θ_est_gpu, θ_est_mat)
+    @test isapprox(θ_est_gpu, θ, atol = T(1e-2))
 end
 
 function generate_affine_data(t::Vector{T}, nr::Int) where {T}
-    Y = fill(T(NaN), (nr, length(t))) # init test data.
-    W = 10 .* randn(2, nr)          # random parameters of affine function.
+    Y = fill(T(NaN), (nr, length(t)))   # init test data.
+    W = 10 .* randn(2, nr)              # random parameters of affine function.
     for i in 1:nr
         Y[i, :] = W[1, i] .* t .+ W[2, i]
     end
@@ -54,13 +53,15 @@ end
     Y, W = generate_affine_data(t, nr)
     Wcpu = ridge_regression(Y, t)
     Wgpu = Array( ridge_regression(CuArray(Y), t) )
-    # println([W, Wcpu, Wgpu])
-    @test sum(isapprox.(W, Wcpu, atol = T(1e-5))) == 2*nr
-    @test sum(isapprox.(W, Wgpu, atol = T(1e-5))) == 2*nr
+    @test sum(isapprox.(W, Wcpu, atol = T(1e-5))) == length(W)
+    @test sum(isapprox.(W, Wgpu, atol = T(1e-5))) == length(W)
 end
 
 @testset "significance" begin
     T = Float32
-    nrows, ncols = 100, 10
-    X = collect(1:nrows) * ones(T, ncols)'
+    nx, ns, nt = 2, 100, 10         # n° of variables, surrogates per variable, time steps per variable.
+    sur_stat = repeat( collect(1:ns) * ones(T, nt)', outer=(2,1) )
+    ref_stat = round.( rand(T, nx, nt) * 100 )
+    significance = percentile_significance(ref_stat, sur_stat, ns, nx)
+    @test significance .* 100 .+ 1 == ref_stat
 end
