d7b2171b8e4221fc3417ee90fc5c2d17b6afbed7
JanJereczek <jan.jereczek@gmail.com>
2022-11-07 19:31:05 +0100

    lastdim

--- src/indicators.jl
+++ src/indicators.jl
@@ -28,27 +28,23 @@ TODO: implement beyond white-noise assumption.
 #####################################################
 
 """@docs
-    mean(X::A) where {A<:Union{Matrix{T}, CuArray{T, 2}}}
+    mean_lastdim(X::A) where {A<:Union{Matrix{T}, CuArray{T, 2}}}
 
 Computes the mean of an array over the last dimension.
 If X is a CuArray, the computation takes place on the GPU.
 """
-function mean(X::Array{T}) where {T<:Real}
-    return StatsBase.mean(X, dims=length(size(X)))
-end
-
-function mean(X::CuArray{T, 2}) where {T<:Real}
+function mean_lastdim(X::A) where{A<:Union{Array{T}, CuArray{T}}} where {T<:Real}
     reduce_dim = length(size(X))
     return reduce( +, X, dims=reduce_dim) ./ T(size(X, reduce_dim))
 end
 
 """@docs
-    masked_mean(X::A) where {A<:Union{Matrix{T}, CuArray{T, 2}}}
+    masked_mean_lastdim(X::A) where {A<:Union{Matrix{T}, CuArray{T, 2}}}
 
 Computes the sliding-window mean of an array over the last dimension by using a mask.
 If X is a CuArray, the computation takes place on the GPU.
 """
-function masked_mean(X::A, M::A) where {A<:Union{Matrix{T}, CuArray{T, 2}}} where {T<:Real}
+function masked_mean_lastdim(X::A, M::A) where {A<:Union{Matrix{T}, CuArray{T, 2}}} where {T<:Real}
     return (X * M) ./ reduce(+, M[:, 1])
 end
 
@@ -63,13 +59,18 @@ function var(X::Array{T}) where {T<:Real}
 end
 
 # Unbiased estimator (consistent with StatsBase).
-function var(X::CuArray{T, 2}, x_mean::CuArray{T, 2}) where {T<:Real}
-    return reduce( +, (X .- x_mean).^2, dims=2) ./ T(size(X, 2) - 1)
+function var(X::CuArray{T, 2}, Xmean::CuArray{T, 2}) where {T<:Real}
+    return reduce( +, (X .- Xmean).^2, dims=2) ./ T(size(X, 2) - 1)
 end
 
 function var(X::CuArray{T, 2}) where {T<:Real}
-    x_mean = mean(X)
-    return var(X, x_mean)
+    Xmean = mean_lastdim(X)
+    return var(X, Xmean)
+end
+
+function biased_var(X::A) where {A<:Union{Matrix{T}, CuArray{T, 2}}} where {T<:Real}
+    n = size(X)[end]
+    return var(X) .* T((n-1) / n)
 end
 
 """@docs
@@ -93,26 +94,15 @@ function skw(x::Vector{T}) where {T<:Real}
     return StatsBase.skewness(x)
 end
 
-function skw(X::Matrix{T}, x_mean::Matrix{T}, x_var::Matrix{T}) where {T<:Real}
-    return reduce( +, (X .- x_mean).^3, dims=2) ./ size(X, 2) ./ (x_var .^ T(1.5))
+function skw(X::A, Xmean::A, Xvar::A) where {A <: Union{Matrix{T}, CuArray{T,2}} } where {T<:AbstractFloat}
+    return reduce( +, (X .- Xmean).^3, dims=2) ./ size(X, 2) ./ (Xvar .^ T(1.5))
 end
 
-function skw(X::Matrix{T}) where {T<:Real}
-    x_mean = StatsBase.mean(X, dims=2)
-    x_var = var(X)
-    return skw(X, x_mean, x_var)
+function skw(X::A) where {A <: Union{Matrix{T}, CuArray{T,2}} } where {T<:AbstractFloat}
+    Xmean = mean_lastdim(X)
+    Xbvar = biased_var(X)
+    return skw(X, Xmean, Xbvar)
 end
-
-function skw(X::CuArray{T, 2}, x_mean::CuArray{T, 2}, x_var::CuArray{T, 2}) where {T<:Real}
-    return reduce( +, (X .- x_mean).^3, dims=2) ./ size(X, 2) ./ (x_var .^ T(1.5))
-end
-
-function skw(X::CuArray{T, 2}) where {T<:Real}
-    x_mean = mean(X)
-    x_var = var(X)
-    return skw(X, x_mean, x_var)
-end
-# TODO compare to skw of StatsBase
 
 """@docs
     krt(X::A) where {A<:Union{Matrix{T}, CuArray{T, 2}}}
@@ -120,30 +110,22 @@ end
 Computes the kurtosis of an array of dimension < 3 over the last dimension.
 If X is a CuArray, the computation takes place on the GPU.
 """
-function krt(x::Vector{T}) where {T<:Real}
+function krt(x::Vector{T}) where {T<:AbstractFloat}
     return StatsBase.kurtosis(x)
 end
 
-function krt(X::Matrix{T}, x_mean::Matrix{T}, x_var::Matrix{T}) where {T<:Real}
-    return reduce( +, (X .- x_mean).^3, dims=2) ./ size(X, 2) ./ (x_var .^ T(1.5))
+function krt(X::A, Xmean::A, Xvar::A) where {A <: Union{Matrix{T}, CuArray{T,2}} } where {T<:AbstractFloat}
+    n = size(X, 2)
+    cor1 = (n+1)*n/(n-1)/(n-2)/(n-3)
+    cor2 = 3 * (n-1)^2/(n-2)/(n-3)
+    return cor1 .* (reduce( +, (X .- Xmean).^4, dims=2) ./ ( (Xvar .^ T(2)) )) .- cor2
 end
 
-function krt(X::Matrix{T}) where {T<:Real}
-    x_mean = StatsBase.mean(X, dims=2)
-    x_var = var(X)
-    return reduce( +, (X .- x_mean).^3, dims=2) ./ size(X, 2) ./ (x_var .^ T(1.5))
+function krt(X::A) where {A <: Union{Matrix{T}, CuArray{T,2}} } where {T<:AbstractFloat}
+    Xmean = mean_lastdim(X)
+    Xvar = var(X)
+    return krt(X, Xmean, Xvar)
 end
-
-function krt(X::CuArray{T, 2}, x_mean::CuArray{T, 2}, x_var::CuArray{T, 2}) where {T<:Real}
-    return reduce( +, (X .- x_mean) .^ 4, dims=2 ) ./ size(X, 2) ./ (x_var .^ 2)
-end
-
-function krt(X::CuArray{T, 2}) where {T<:Real}
-    x_mean = mean(X)
-    x_var = var(X)
-    return krt(X, x_mean, x_var)
-end
-# TODO compare to krt of StatsBase
 
 #####################################################
 # Analytic regression models
@@ -161,12 +143,7 @@ function ar1_whitenoise(x::Vector{T}) where {T<:Real}
 end
 
 # AR1 coefficients for a vertical stack of residuals X and white noise assumption.
-function ar1_whitenoise(X::Array{T}) where {T<:Real}
-    return reduce( +, X[:, 2:end] .* X[:, 1:end-1], dims=2) ./ reduce( +, X[:, 1:end-1] .* X[:, 1:end-1], dims=2)
-end
-
-# GPU accelerated AR1 coefficients for a vertical stack of time series X for white noise assumption.
-function ar1_whitenoise(X::CuArray{T, 2}) where {T<:Real}
+function ar1_whitenoise(X::A) where {A<:Union{Matrix{T}, CuArray{T,2}}} where {T<:Real}
     return reduce( +, X[:, 2:end] .* X[:, 1:end-1], dims=2) ./ reduce( +, X[:, 1:end-1] .* X[:, 1:end-1], dims=2)
 end
 
@@ -176,7 +153,7 @@ end
 Computes the same as [`ar1_ar1noise`](@ref ar1_whitenoise) but by means of a masking matrix.
 This provides a significant speed-up for large data sets that are computed on GPU.
 """
-function masked_ar1_whitenoise(X::CuArray{T, 2}, M::CuArray{T, 2}) where {T<:Real}
+function masked_ar1_whitenoise(X::A, M::A) where {A <: Union{SparseMatrixCSC{T, Int}, CuArray{T, 2}}}  where {T<:Real} 
     return ((X[:, 2:end] .* X[:, 1:end-1]) * M[1:end-1, :]) ./ ((X[:, 1:end-1] .* X[:, 1:end-1]) * M[1:end-1, :])
 end
 
