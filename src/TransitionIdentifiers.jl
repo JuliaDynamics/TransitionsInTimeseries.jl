6b5e988e64e10dbb265fd6c2391f0b7aed43df5f
JanJereczek <jan.jereczek@gmail.com>
2022-11-08 18:13:41 +0100

    reduce import + export new functions

--- src/TransitionIdentifiers.jl
+++ src/TransitionIdentifiers.jl
@@ -4,7 +4,7 @@ using LinearAlgebra
 using CUDA
 using FFTW: rfft, irfft
 using StatsBase: corkendall
-using SparseArrays: spzeros, spdiagm
+using SparseArrays: spzeros, spdiagm, SparseMatrixCSC
 
 include("utils.jl")
 include("signal_processing.jl")
@@ -13,37 +13,84 @@ include("indicators.jl")
 include("trend_estimation.jl")
 include("significance.jl")
 include("indicate_transition.jl")
-# include("benchmark.jl")
+
+#############################
+# Utils
+#############################
+
+export Residual
+export structured_residual
+export flatten_residual
+export reshape_residual
+export get_placeholder_spacedims
+
+#############################
+# Signal processing
+#############################
 
 export get_windowing_params
-export centered_wndw, left_wndw, right_wndw, trim_wndw
+export centered_wndw, left_wndw, right_wndw
+export trim_wndw
 export slide_estimator
-export gettrend_rollmean, get_trend, detrend
-export window_mask, strided_window_mask
+export grow_window
+export gettrend_rollmean
+export gettrend_rollkernel
+export get_trend
+export detrend
+export window_mask
+export strided_window_mask
 
-export uniform_kernel, triangular_kernel, parabolic_kernel
-export biweight_kernel, triweight_kernel, tricube_kernel
-export gaussian_kernel, cosine_kernel, logistic_kernel
-export sigmoid_kernel, scaled_kernel
+#############################
+# Kernels
+#############################
 
-export mean_lastdim, masked_mean_lastdim
-export var, masked_meansquare, std
-export skw, krt
-export ar1_whitenoise, masked_ar1_whitenoise
+export uniform_kernel
+export triangular_kernel
+export parabolic_kernel
+export biweight_kernel
+export triweight_kernel
+export tricube_kernel
+export gaussian_kernel
+export cosine_kernel
+export logistic_kernel
+export sigmoid_kernel
+export scaled_kernel
+
+#############################
+# Indicators
+#############################
+
+export mean_lastdim
+export masked_mean_lastdim
+export var
+export masked_meansquare
+export std
+export skw
+export krt
+export ar1_whitenoise
+export masked_ar1_whitenoise
+export arp_whitenoise
 export hurst_exponent
 export lfps
 
+#############################
+# Significance
+#############################
+
 export generate_stacked_fourier_surrogates
-export ridge_regression, ridge_regression_slope
-export kendall_tau, scaled_kendall_tau
+export ridge_regression
+export ridge_regression_slope
+export kendall_tau
+export scaled_kendall_tau
 export percentile_significance
-export slide_idtrend
+export slide
 export count_positive_indicators
+export stack_indicators
 
-export stack_indicators, indicate_transition
+#############################
+# High-level interface
+#############################
 
-export benchmark_functions_over_size
-export benchmark_cpu_vs_gpu
-export lineplot_benchmark
+export indicate_transition
 
 end # module TransitionIdentifiers
