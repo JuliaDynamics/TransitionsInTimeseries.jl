b4420656f575f414a8b9818315f4b9cd80cecfe0
JanJereczek <jan.jereczek@gmail.com>
2022-11-11 13:29:10 +0100

    remove dummy

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,5 @@ defaultname(file) = uppercasefirst(replace(splitext(basename(file))[1], '_' => '
 testfile(file, testname=defaultname(file)) = @testset "$testname" begin; include(file); end
 
 @testset "TranstionIndicators.jl" begin
-    testfile("dummy.jl")
     testfile("windowing.jl")
 end
