80c26dd45022014c5a0b7ea9c2665b77e1105a22
JanJereczek <jan.jereczek@gmail.com>
2022-11-15 16:26:56 +0100

    corrected docstring of WindowViewer

--- src/windowing.jl
+++ src/windowing.jl
@@ -13,11 +13,7 @@ end
         stride::Int,
     ) where {T<:Real, V<:AbstractVector{<:T}}
 
-Returns struct of WindowViewer with fields:
-- `WindowViewer.timeseries`
-- `WindowViewer.halfwidth`
-- `WindowViewer.stride`
-- `WindowViewer.strided_indices`: a vector of indices at which the window viewing should be applied.
+Initialize an iterator that generates views over the given timeseries based on a window with a given `halfwidth`, incrementing the window views with the given `stride`. You can use this directly with `map`, such as `map(std, WindowViewer(x, ...))` would give you the moving-window-timeseries of the `std` of `x`.
 """
 function WindowViewer(
     timeseries::V,
@@ -32,17 +28,9 @@ end
 
 """
 
-    bracket(idx::Int, wv::WindowViewer)
-
-Returns index span based on target index and parameters of the window viewer.
-"""
-bracket(idx::Int, wv::WindowViewer) = (idx-2*wv.halfwidth, idx)
-
-"""
-
     get_stride_indices(l::Int, halfwidth::Int, stride::Int)
 
-Returns a vector with strided indices based on windowing parameters.
+Return a vector with strided indices based on windowing parameters.
 """
 get_stride_indices(l::Int, halfwidth::Int, stride::Int) = 2*halfwidth+1:stride:l
 
@@ -53,8 +41,8 @@ function Base.iterate(wv::WindowViewer, state::Int = 1)
         return nothing
     else
         k = wv.strided_indices[state]
-        i1, i2 = bracket(k, wv)
-        return (view(wv.timeseries, i1:i2), state+1)    # Else: return a view of time series.
+        i1, i2 = (k-2*wv.halfwidth, k)
+        return (view(wv.timeseries, i1:i2), state + 1)  # Else: return a view of time series.
     end
 end
 Base.eltype(::Type{WindowViewer}) = AbstractVector{<:Real}
