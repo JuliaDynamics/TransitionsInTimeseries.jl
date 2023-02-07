e2e16478f25ee5ad9211253d028047c67fd92501
JanJereczek <jan.jereczek@gmail.com>
2023-02-07 17:44:03 +0100

    reexport tssurrogates & statsbase

--- src/TransitionIndicators.jl
+++ src/TransitionIndicators.jl
@@ -1,7 +1,12 @@
 module TransitionIndicators
 
+using Reexport
+@reexport using StatsBase
+@reexport using TimeseriesSurrogates
+
 include("windowing.jl")
 include("indicators.jl")
+include("indicator_metrics.jl")
 
 export WindowViewer
 export ar1_whitenoise
