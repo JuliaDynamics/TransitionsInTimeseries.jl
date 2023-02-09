3876afaefc763058642f095bacc95c841674584e
JanJereczek <jan.jereczek@gmail.com>
2023-02-09 10:33:58 +0100

    added gaussian_quantile and which_quantile

--- src/significance.jl
+++ src/significance.jl
@@ -8,11 +8,27 @@ analysis.
 @inline function measure_significance(
     res::IndicatorEvolutionResults{T},
     significance_metric::Function,
-) where{T<:AbstractFloat}
-    significances = similar(res.x_evolution)
-    for j in eachindex(significances)
-        significances[j] = significance_metric(x[j], res.surr_evolutions[:, j])
+) where {T<:AbstractFloat}
+    return measure_significance(
+        res.x_evolution,
+        res.surr_x_evolution,
+        significance_metric,
+    )
+end
+
+@inline function measure_significance(
+    x_evolution::AbstractVector{T},
+    surr_x_evolution::AbstractMatrix{T},
+    significance_metric::Function,
+) where {T<:AbstractFloat}
+    significance = similar(x_evolution)
+    for j in eachindex(significance)
+        significance[j] = significance_metric(
+            x_evolution[j],
+            view(surr_x_evolution, :, j),
+        )
     end
+    return significance
 end
 
 """
@@ -21,33 +37,63 @@ end
 
 Compute the indices of the quantile values within the vector `s`.
 """
-@inline function get_quantile_idx(
+@inline function quantile_idx(
     s::AbstractVector{T};
     q::T = T(0.95),
     symmetric::Bool = false,
 ) where{T<:AbstractFloat}
+    n = length(s)
     sorted_idx = sortperm(s)
-    i_lo = symmetric ? sorted_idx[roundint(n * (1 - q))] : 1
-    i_hi = sorted_idx[roundint(n * q)]
+    i_lo = symmetric ? sorted_idx[intround(n * (1 - q))] : sorted_idx[1]
+    i_hi = sorted_idx[intround(n * q)]
     return i_lo, i_hi
 end
 
 """
 
-    get_normalized_interquantile(x, s)
+    normalized_quantile_distance(x, s)
 
 Compute the normalized distance between an input value `x` and the
-quantiles of a vector `s`.
+quantiles of a vector `s`. 
 """
-@inline function get_normalized_interquantile(
+@inline function normalized_quantile_distance(
     x::T,
     s::AbstractVector{T};
     q::T = T(0.95),
     symmetric::Bool = false,
 ) where{T<:AbstractFloat}
-    i_lo, i_hi = get_quantile_idx(s, q=q, symmetric=symmetric)
-    s_mi = 0.5 * (s[i_lo] + s[i_hi])
-    return (x - s_mi) / s_mi
+    i_lo, i_hi = quantile_idx(s, q=q, symmetric=symmetric)
+    center = 0.5 * (s[i_lo] + s[i_hi])
+    interq = 0.5 * (s[i_hi] - s[i_lo])
+    return (x - center) / abs(interq)
 end
 
-roundint(x::Real) = round(Int(x))
+"""
+
+    gaussian_quantile(x, s)
+
+Compute the normalized distance between an input value `x` and the
+quantiles of a vector `s`. 
+"""
+@inline function gaussian_quantile(
+    x::T,
+    s::AbstractVector{T};
+    nstd::T=2.0,
+) where{T<:AbstractFloat}
+    return T( (x - mean(s)) / (nstd * std(s)) )
+end
+
+"""
+
+    which_quantile(x, s)
+
+Compute the quantile represented by the input value `x` w.r.t. the vector `s`. 
+"""
+@inline function which_quantile(
+    x::T,
+    s::AbstractVector{T},
+) where{T<:AbstractFloat}
+    return sum(x .> s) / length(s)
+end
+
+intround(x::Real) = Int(round(x))
