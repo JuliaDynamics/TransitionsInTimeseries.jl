e046c28ce359707fee1b93abfd8d8a13b94cfd04
JanJereczek <jan.jereczek@gmail.com>
2023-02-17 16:44:53 +0100

    precompute ridge

--- src/metaanalysis_trend.jl
+++ src/metaanalysis_trend.jl
@@ -34,7 +34,6 @@ function precompute_ridge(
     return inv(TT * TT' + lambda .* I(2) ) * TT
 end
 
-
 """
 
     ridge_slope(x, y; lambda)
@@ -42,3 +41,47 @@ end
 Extract the slope of ridge regression of `y` over `x`.
 """
 ridge_slope(args...; kwargs...) = ridge(args...; kwargs...)[1]
+
+"""
+
+    ridge_slope(t, x; lambda)
+
+Extract the slope of ridge regression of `x` over `t`.
+"""
+ridge_slope(args...; kwargs...) = ridge(args...; kwargs...)[1]
+
+"""
+
+    precomputed_ridge_slope(x, m)
+
+Extract the slope of ridge regression of `x` based on precomputed ridge vector `m`.
+"""
+function precomputed_ridge_slope(
+    x::AbstractVector{T},
+    m::AbstractVector{T},
+) where {T<:AbstractFloat} 
+    return m' * x
+end
+
+
+"""
+    precompute_ridge(t, lambda = 0)
+
+Precompute the matrix arising in the ridge regression. Particularly suited
+if ridge regression is to be computed many times on evenly spaced time.
+"""
+function precompute_ridge(
+    t::AbstractVector{T};
+    lambda::T = T(0),
+) where {T<:Real}
+    TT = hcat(t, ones(T, length(t)))'
+    return inv(TT * TT' + lambda .* LinearAlgebra.I(2) ) * TT
+end
+
+"""
+    precompute_ridge_slope(t, lambda = 0)
+
+Precompute the vector arising in the ridge regression of the slope. Particularly suited
+if ridge regression is to be computed many times on evenly spaced time.
+"""
+precompute_ridge_slope(args...; kwargs...) = precompute_ridge(args...; kwargs...)[1,:]
