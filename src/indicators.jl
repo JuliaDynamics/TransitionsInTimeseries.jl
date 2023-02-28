a55cd061003ece60921a4c9972e6a02ea80c4e5c
JanJereczek <jan.jereczek@gmail.com>
2023-02-28 15:26:08 +0100

    export moments from Statsbase

--- src/indicators.jl
+++ src/indicators.jl
@@ -14,3 +14,33 @@ function ar1_whitenoise(x::AbstractVector{T}) where {T<:Real}
     n = length(x)
     return (view(x, 2:n)' * view(x, 1:n-1)) / (view(x, 1:n-1)' * view(x, 1:n-1))
 end
+
+"""
+
+    variance(x)
+
+Compute variance of `x`.
+"""
+function variance(args...; kwargs...)
+    return var(args...; kwargs...)
+end
+
+"""
+
+    skw(x)
+
+Compute skewness of `x`.
+"""
+function skw(args...; kwargs...)
+    return skewness(args...; kwargs...)
+end
+
+"""
+
+    krt(x)
+
+Compute kurtosis of `x`.
+"""
+function krt(args...; kwargs...)
+    return kurtosis(args...; kwargs...)
+end
