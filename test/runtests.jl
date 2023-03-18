7bbc0729317b2cae4b0697bac5c01d466649cfa1
JanJereczek <jan.jereczek@gmail.com>
2023-03-18 23:34:56 +0100

    include all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,9 @@ defaultname(file) = uppercasefirst(replace(splitext(basename(file))[1], '_' => '
 testfile(file, testname=defaultname(file)) = @testset "$testname" begin; include(file); end
 
 @testset "TranstionIndicators.jl" begin
+    testfile("timeseries.jl")
     testfile("windowing.jl")
     testfile("indicators.jl")
-    testfile("metaanalysis_trend.jl")
+    testfile("change_metrics.jl")
     testfile("significance.jl")
 end
--- test/timeseries.jl
+++ test/timeseries.jl
@@ -9,11 +9,13 @@ using TransitionIndicators, Test
     @test equispaced_step(x_equispaced) ≈ dt
 end
 
-# Why not use this?
+#=
+# Why not use this? Slightly more succinct and performant.
 function my_equispaced_step(x::AbstractVector)
     return mean(diff(x))
 end
 
 x_equispaced = collect( range( 0.0, step = rand(), stop = 100) )
-@btime equispaced_step(x_equispaced)
-@btime my_equispaced_step(x_equispaced)
+@btime equispaced_step(x_equispaced)        # 201 ns
+@btime my_equispaced_step(x_equispaced)     # 178 ns
+=#
