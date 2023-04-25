be1e45356ac1ee005d088d2dd868f9a05caf052d
JanJereczek <jan.jereczek@gmail.com>
2023-04-25 11:13:46 +0200

    test ridge regression

--- test/change_metrics.jl
+++ test/change_metrics.jl
@@ -1,55 +1,37 @@
-using TransitionIndicators, Test, Random, TimeseriesSurrogates
-using Distributions
+using TransitionIndicators, Test, Random, Distributions
 
-# @testset "checking slope estimation" begin
-#     d = Normal()                # define normal distribution
-#     m, p = 10 .* rand(d, 2)     # slope and offset parameters
-#     x = 0:0.1:10
-#     y = m .* x .+ p             # define affine function
-#     y_noisy = y + 0.1 .* rand(d, length(x))
-#     rr = RidgeRegression(x)
-#     m_est = rr(y)
-#     m_est_noisy = rr(y_noisy)
-#     @test isapprox(m, m_est)
-#     @test isapprox(m, m_est_noisy, atol = 1e-1)
-# end
+@testset "ridge regression" begin
+    d = Normal()                # define normal distribution
+    m, p = 10 .* rand(d, 2)     # slope and offset parameters
+    t = 0:0.1:100
+    x = m .* t .+ p             # define affine function
+    x_noisy = x + 0.1 .* rand(d, length(x))
 
-d = Normal()                # define normal distribution
-m, p = 10 .* rand(d, 2)     # slope and offset parameters
-t = 0:0.1:100
-x = m .* t .+ p             # define affine function
-noise = 0.1 .* rand(d, length(x))
-x_noisy = x + noise
+    indconfig = IndicatorsConfig(t, [var], width = 20, stride = 1)
+    sigconfig = SignificanceConfig(indconfig, [RidgeRegression], width = 100, stride = 1)
 
-iparams = IndicatorsParams(q_lofreq = 0.1)
-indconfig = IndicatorsConfig(t, [ar1_whitenoise, LowfreqPowerSpectrum],
-    width = 100, stride = 2)
+    x_test = x[indconfig.width:indconfig.stride:end]
+    x_test_noisy = x_noisy[indconfig.width:indconfig.stride:end]
+    m_hat = sigconfig.change_metrics[1](x_test[1:sigconfig.width])
+    m_hat_noisy = sigconfig.change_metrics[1](x_test_noisy[1:sigconfig.width])
+    @test isapprox(m_hat, m, atol = 1e-5)
+    @test isapprox(m_hat_noisy, m, atol = 1e-2)
+end
 
-cmparams = ChangeMetricsParams(lambda = 0.0)
-sigconfig = SignificanceConfig(
-    indconfig,
-    [RidgeRegression, kendalltau],
-    change_metrics_params = cmparams,
-    width = 10,
-    stride = 1,
-)
 
-lfps_test = indconfig.indicators[2](noise[1:indconfig.width])
-x_indicator = x[indconfig.width:indconfig.stride:end]
-sigconfig.change_metrics[1](x_indicator[1:sigconfig.width])
+@testset "sliding ridge regression" begin
+    n = 1001
+    t = collect(1.0:n)
+    x = copy(t)
 
-# @testset "sliding trend estimation over indicator" begin
-#     n = 1001
-#     t = collect(1.0:n)
-#     x = copy(t)
-#     p = SignificanceHyperParams(n_surrogates = 100)
-#     rr = RidgeRegression(t, p.wv_evolution_width)
-#     res = analyze_indicators(t, x, [mean, var], rr, p)
+    indconfig = IndicatorsConfig(t, [mean, var], width = 100, stride = 1)
+    sigconfig = SignificanceConfig(indconfig, [RidgeRegression], width = 100, stride = 1)
+    res = indicators_analysis(t, x, indconfig, sigconfig)
 
-#     # The trend of mean(windowview) is the stride for x=t
-#     meantrend_ground_truth = fill(p.wv_indicator_stride, length(res.t_evolution))
-#     # The trend of var(windowview) is 0 for x any affine function of t.
-#     vartrend_ground_truth = fill(0.0, length(res.t_evolution))
-#     @test isapprox(res.X_evolution[1, :, 1], meantrend_ground_truth)
-#     @test isapprox(res.X_evolution[1, :, 2], vartrend_ground_truth, atol = 1e-12)
-# end
+    # The trend of mean(windowview) is the stride for x=t
+    meantrend_ground_truth = fill(indconfig.stride, length(res.t_change))
+    # The trend of var(windowview) is 0 for x any affine function of t.
+    vartrend_ground_truth = fill(0.0, length(res.t_change))
+    @test isapprox(res.x_change[:, 1], meantrend_ground_truth, atol = 1e-9)
+    @test isapprox(res.x_change[:, 2], vartrend_ground_truth, atol = 1e-9)
+end
