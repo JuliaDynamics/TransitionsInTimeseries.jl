bb22edeab4aafbe0e95155f18f1cfe2212299fb7
JanJereczek <jan.jereczek@gmail.com>
2023-05-24 21:55:05 +0200

    docstrings for default choices of width and stride

--- src/misc/params.jl
+++ src/misc/params.jl
@@ -6,6 +6,11 @@ const DEFAULT_N_SURROGATES = 10_000
 const DEFAULT_SURROGATE_METHOD = RandomFourier()
 const DEFAULT_WINDOW_STRIDE = 1
 
+"""
+    default_window_width(x)
+
+Return the default window width chosen to be `length(x) ÷ 100` and at least `10`.
+"""
 function default_window_width(x)
     w = max(length(x)÷100, 10)
     if w < 50
--- src/misc/windowing.jl
+++ src/misc/windowing.jl
@@ -13,7 +13,8 @@ window with a given `width`, incrementing the window views with the given
 `stride`. You can use this directly with `map`, such as `map(std, WindowViewer(x, ...))`
 would give you the moving-window-timeseries of the `std` of `x`.
 
-If not given, the keywords `width, stride` as taken as `length(x)÷100` and `1`.
+If not given, the keywords `width, stride` are taken as
+[`default_window_width(x)`](@ref) and `1`.
 """
 function WindowViewer(
         x::AbstractVector;
