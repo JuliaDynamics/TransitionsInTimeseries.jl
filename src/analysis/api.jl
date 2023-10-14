8142316060bc29f80d5c1845682ece7427ffe9e0
Datseris <datseris.george@gmail.com>
2023-10-14 10:33:02 +0200

    add generic docstring to main API function

--- src/analysis/api.jl
+++ src/analysis/api.jl
@@ -8,3 +8,16 @@ Supertype used to define how indicators and their changes are estimated in
  - [`SegmentedWindowConfig`](@ref).
 """
 abstract type IndicatorsChangesConfig end
+
+"""
+    estimate_indicator_changes(config::IndicatorsChangesConfig, x [,t]) → output
+
+Estimate possible transitions for input timeseries `x` using the approach specified
+in the configuration type `config`, see [`IndicatorsChangesConfig`](@ref) for possibilities.
+`t` is the time vector corresponding to `x`, which defaults to `eachindex(x)`.
+
+Return the output as [`WindowResults`](@ref) which can be given to
+[`significant_transitions`](@ref) to deduce which possible transitions are statistically
+significant using a variety of significance tests.
+"""
+function estimate_indicator_changes end
