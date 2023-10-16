15156be583108555a6634ecffda356f88c42a5c6
JanJereczek <jan.jereczek@gmail.com>
2023-10-16 16:38:48 +0200

    reducing mem alloc when computing pvals

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -36,3 +36,37 @@ Output on Jan's machine:
 
 # Check 0 allocations
 @btime inplace_map!(ar1_whitenoise, $var_x, $wv)
+
+
+#################################################
+# surrogates_loop!
+#################################################
+
+using TransitionsInTimeseries
+
+n = 1001
+t = collect(1.0:n)
+x = copy(t)
+
+indicators = (mean, var)
+change_metrics = RidgeRegressionSlope()
+
+config = SlidingWindowConfig(indicators, change_metrics;
+    width_ind = 100, stride_ind = 1,
+    width_cha = 100, stride_cha = 1, whichtime = last,
+)
+
+res = estimate_indicator_changes(config, x, t)
+signif = SurrogatesSignificance(n = 100, tail = :both, p = 0.1)
+flags = significant_transitions(res, signif)
+
+@btime estimate_indicator_changes($config, $x, $t)
+# 141.557 μs (4 allocations: 40.56 KiB)
+@btime significant_transitions($res, $signif)
+#=
+Former version of the code: 5.178 ms (1954 allocations: 3.03 MiB)
+Current version of the code: 4.863 ms (738 allocations: 2.96 MiB)
+
+Although there is an improvement in the number of allocations, the performance
+is not significantly improved.
+=#
