4084fbdc61b240c115d05f3cd7f616f900eb6536
JanJereczek <jan.jereczek@gmail.com>
2024-01-25 11:08:39 +0100

    plot_changes_significance works with kwargs

--- ext/TransitionVisualizations.jl
+++ ext/TransitionVisualizations.jl
@@ -3,8 +3,8 @@ module TransitionVisualizations
 using TransitionsInTimeseries, Makie
 
 # Default options for plotting utilities
-global default_accent_linewidth = 3
-global default_colors = ["#7143E0", "#0A9A84", "#191E44", "#AF9327", "#701B80", "#2E6137"]
+const default_accent_linewidth = 3
+const default_colors = ["#7143E0", "#0A9A84", "#191E44", "#AF9327", "#701B80", "#2E6137"]
 
 default_indicator_label(res::IndicatorsChangesResults) = [string(nameof(
     res.config.indicators[i])) for i in eachindex(res.config.indicators)]
@@ -22,21 +22,40 @@ function default_chametric_label(res::IndicatorsChangesResults)
     return labels
 end
 
-# Struct to help pass results
+"""
+    TransitionVisualization
+Contains:
+ - `fig::Figure` the figure,
+ - `laxs::Vector{Axis}` the left axes,
+ - `raxs::Vector{Axis}` the right axes,
+ - `res::IndicatorsChangesResults` the results of the input signal,
+ - `colors::Vector` a vector containing the colors to use for plotting.
+"""
 struct TransitionVisualization{R<:IndicatorsChangesResults}
     fig::Makie.Figure
     laxs::Vector{Makie.Axis}
     raxs::Vector{Makie.Axis}
     res::R
+    colors::Vector
 end
 
-# plot_indicator_changes
+export TransitionVisualization
+
+# Plot results from full analysis
+function TransitionsInTimeseries.plot_changes_significance(res, signif; kwargs...)
+    tv = plot_indicator_changes(res; kwargs...)
+    plot_significance!(tv, signif; kwargs...)
+    return tv
+end
+
+# Plot results of original input signal
 function TransitionsInTimeseries.plot_indicator_changes(res::SlidingWindowResults;
         colors = default_colors,
         indicator_names = default_indicator_label(res),
         chametric_names = default_chametric_label(res),
         additional_timeseries = nothing,
         accent_linewidth = default_accent_linewidth,
+        kwargs...,
     )
 
     fig, laxs, raxs, n, config = init_rowwise_visualisation(res, colors, indicator_names,
@@ -44,7 +63,7 @@ function TransitionsInTimeseries.plot_indicator_changes(res::SlidingWindowResult
     lineplot_metrics!(raxs, laxs, n, config, res.t_indicator, res.x_indicator,
         res.t_change, res.x_change, colors, accent_linewidth)
 
-    return TransitionVisualization(fig, laxs, raxs, res)
+    return TransitionVisualization(fig, laxs, raxs, res, colors)
 end
 
 function TransitionsInTimeseries.plot_indicator_changes(res::SegmentedWindowResults;
@@ -52,7 +71,9 @@ function TransitionsInTimeseries.plot_indicator_changes(res::SegmentedWindowResu
     indicator_names = default_indicator_label(res),
     chametric_names = default_chametric_label(res),
     additional_timeseries = nothing,
-    accent_linewidth = default_accent_linewidth)
+    accent_linewidth = default_accent_linewidth,
+    kwargs...,
+    )
 
     fig, laxs, raxs, n, config = init_rowwise_visualisation(res, colors, indicator_names,
         chametric_names, additional_timeseries, accent_linewidth)
@@ -60,11 +81,10 @@ function TransitionsInTimeseries.plot_indicator_changes(res::SegmentedWindowResu
         Makie.lines!(laxs[1], res.t[k], res.x[k], color = colors[1],
             linewidth = accent_linewidth)
         lineplot_metrics!(raxs, laxs, n, config, res.t_indicator[k], res.x_indicator[k],
-            res.t_change[k], res.x_change[k, :],
-            colors, accent_linewidth)
+            res.t_change[k], res.x_change[k, :], colors, accent_linewidth)
     end
 
-    return TransitionVisualization(fig, laxs, raxs, res)
+    return TransitionVisualization(fig, laxs, raxs, res, colors)
 end
 
 # utils for plot_indicator_changes
@@ -95,12 +115,12 @@ function init_rowwise_visualisation(res, colors, indicator_names,
 
     elements = [LineElement(color = (colors[1], transparency), linewidth = lw) for 
         (lw, transparency) in [(accent_linewidth, 1), (1, 0.5)]]
-    labels = ["original signal", "surrogate signals"]
+    labels = ["original signal", "surro signals"]
     width = 0.5
     if length(res.t_indicator[1]) > 1
         elements = vcat(elements, [MarkerElement(marker = :circle, color = colors[1],
             strokecolor = :transparent, markersize = ms) for ms in [10, 5]])
-        labels = vcat(labels, ["original change metric", "surrogate change metrics"])
+        labels = vcat(labels, ["original change metric", "surro change metrics"])
         width = 1
     end
     Legend(fig[0, 1], elements, labels, nbanks = 4, rowgap = 0, colgap = 10,
@@ -127,16 +147,16 @@ function lineplot_metrics!(raxs, laxs, n, config, t_ind, x_ind, t_cha, x_cha,
     end
 end
 
-# plot_significance!
+# Plot results of surrogates
 function TransitionsInTimeseries.plot_significance!(
     tv::TransitionVisualization{<:SlidingWindowResults},
     signif::SurrogatesSignificance;
     flags = nothing,
-    colors = default_colors,
     nsurro = 20,
+    kwargs...,
     )
 
-    (; fig, laxs, raxs, res) = tv
+    (; fig, laxs, raxs, res, colors) = tv
     config = res.config
     lines_over_surro!(raxs, laxs, nsurro, res.t, res.t_indicator, res.t_change, res.x,
         signif, config, flags, colors)
@@ -147,15 +167,22 @@ function TransitionsInTimeseries.plot_significance!(
     tv::TransitionVisualization{<:SegmentedWindowResults},
     signif::SurrogatesSignificance;
     flags = nothing,
-    colors = default_colors,
     nsurro = 20,
+    kwargs...,
     )
 
-    (; fig, laxs, raxs, res) = tv
+    (; fig, laxs, raxs, res, colors) = tv
     config = res.config
     for k in eachindex(res.t_indicator)
-        lines_over_surro!(raxs, laxs, nsurro, res.t[res.i1[k]:res.i2[k]], res.t_indicator[k],
-            res.t_change[k], res.x[res.i1[k]:res.i2[k]], signif, config, flags[k, :], colors)
+        if isnothing(flags)
+            lines_over_surro!(raxs, laxs, nsurro, res.t[res.i1[k]:res.i2[k]],
+                res.t_indicator[k], res.t_change[k], res.x[res.i1[k]:res.i2[k]],
+                signif, config, nothing, colors)
+        else
+            lines_over_surro!(raxs, laxs, nsurro, res.t[res.i1[k]:res.i2[k]],
+                res.t_indicator[k], res.t_change[k], res.x[res.i1[k]:res.i2[k]],
+                signif, config, flags[k, :], colors)
+        end
     end
     return tv
 end
@@ -197,12 +224,6 @@ function lines_over_surro!(raxs, laxs, nsurro, t, t_ind, t_cha, x, signif, confi
             end
         end
     end
-end
-
-function plot_changes_significance(res, signif)
-    tv = plot_indicator_changes(res)
-    plot_significance!(tv, signif)
-    return tv
 end
 
 end
