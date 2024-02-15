a80ff1f89cb1e891d1fd80f4dfad7e079a45fa8d
Datseris <datseris.george@gmail.com>
2024-02-15 13:47:57 +0000

    finish slope change code

--- src/TransitionsInTimeseries.jl
+++ src/TransitionsInTimeseries.jl
@@ -26,6 +26,7 @@ include("misc/precomputation.jl")
 include("analysis/api.jl")
 include("analysis/sliding_window.jl")
 include("analysis/segmented_window.jl")
+include("analysis/slope_change.jl")
 include("significance/api_significance.jl")
 include("significance/surrogates_significance.jl")
 include("significance/basic_stat_significance.jl")
@@ -60,6 +61,7 @@ export SlidingWindowResults, SegmentedWindowResults
 export estimate_changes, ChangesResults
 export Significance, significant_transitions, segmented_significance
 export ThresholdSignificance, QuantileSignificance, SigmaSignificance, SurrogatesSignificance
+export SlopeChangeConfig, SlopeChangeResults
 
 # timeseries
 export isequispaced, equispaced_step
--- src/analysis/slope_change.jl
+++ src/analysis/slope_change.jl
@@ -7,13 +7,18 @@ struct SlopeChangeConfig{I} <: ChangesConfig
     whichtime::W
 end
 
-struct SlopeChangeResults{T} <: ChangesResults
+struct SlopeChangeResults{
+        TT, T<:Real, X<:Real, XX<:AbstractVector{X}, IT,
+        W, L
+    } <: ChangesResults
     t::T # original time vector
     x::X # original timeseries
     t_indicator::IT
     x_indicator::IX
-    t_change::Float64 # only 1 change exists
+    t_change::Float64
     fitparams::Vector{Float64}
+    config::W
+    lsqfit::L
 end
 
 function estimate_changes(config::ChangesConfig, x, t = eachindex(x))
@@ -23,16 +28,36 @@ function estimate_changes(config::ChangesConfig, x, t = eachindex(x))
         # Skip indicators if they are nothing
         t_indicator = t
     else
-        t_indicator = windowmap(config.whichtime, t; width = config.width_ind,
-            stride = config.stride_ind)
+        t_indicator = windowmap(config.whichtime, t;
+            width = config.width_ind, stride = config.stride_ind
+        )
     end
-
     p0 = guess_initial_p(x, t)
     fit = LsqFit.curve_fit(twolinear, t_indicator, x_indicator, p0)
     pbest = LsqFit.coef(fit)
+    a, b, c, d = pbest
+    t_change = (c - a)/(b - d)
+    return SlopeChangeResults(t, x, t_indicator, x_indicator, t_change, pbest, config, fit)
+end
 
-    t_change = (a, b, c, d = pbest; (c - a)/(b - d))
-    return SlopeChangeResults(t, x, t_indicator, x_indicator, t_change, pbest)
+function guess_initial_p(x, t)
+    midindex = (firstindex(x) + lastindex(x))÷2
+    x1 = x[firstindex(x):midindex]
+    x2 = x[midindex:lastindex(x)]
+    t1 = t[firstindex(x):midindex]
+    t2 = t[midindex:lastindex(x)]
+    a, b = linreg(t1, x1)
+    c, d = linreg(t2, x2)
+    return [a, b, c, d]
+end
+
+import Statistics
+function linreg(x, y)
+    mx = Statistics.mean(x)
+    my = Statistics.mean(y)
+    b = Statistics.covm(x, mx, y, my)/Statistics.varm(x, mx)
+    a = my - b*mx
+    return a, b
 end
 
 function twolinear(t, p)
