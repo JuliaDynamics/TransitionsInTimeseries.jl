393c29eaa663b688c577595d6679abc86d9408ea
Datseris <datseris.george@gmail.com>
2025-05-21 10:39:50 +0100

    move to v1

--- src/analysis/slope_change.jl
+++ src/analysis/slope_change.jl
@@ -9,7 +9,11 @@ It estimates a change of slope in the timeseries by fitting two
 connected linear segments to the timeseries,
 returning the results (i.e., the two-linear fits) as [`SlopeChangeResults`](@ref).
 
+Currently, significance of `SlopeChangeConfig` results can only be
+checked with [`SlopeChangeSignificance`](@ref).
+
 ## Keyword arguments
+
 - `indicator = nothing`: a function f(x) -> Real.
   The slope fitting is then done over an indicator of the timeseries, which itself
   is estimated via a sliding window exactly as in [`SlidingWindowConfig`](@ref).
